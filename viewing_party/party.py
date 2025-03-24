39edf6ed516d5a9147f34248f22fb180b042fa80
jenny xu <jingwenxu13@gmail.com>
2025-03-24 13:36:39 -0700

    implements Wave2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -55,7 +55,48 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    total_rating = 0
+    if len(user_data["watched"]) == 0:
+        return 0.0
+    for movie in user_data["watched"]:
+        # print (movie["rating"])
+        total_rating += movie["rating"]   
+        # print(total_rating)
+    avg_rating = total_rating / len(user_data["watched"])
+    return avg_rating
+    
+def get_most_watched_genre(user_data):
+    if len(user_data["watched"]) == 0:
+        return None
+    genre_frequence = {}
+    for movie in user_data["watched"]:
+        if movie["genre"] in genre_frequence:
+            genre_frequence[movie["genre"]] +=1
+            # print(genre_frequence)
+        else:
+            genre_frequence[movie["genre"]] = 1
+    max_frequence = max(genre_frequence, key = genre_frequence.get)
+    # print(genre_frequence)
 
+    return max_frequence
+
+
+
+user_data = {
+        "watchlist": [{
+            "title": "MOVIE_TITLE_1",
+            "genre": "GENRE_1",
+            "rating": 5
+        }],
+        'watched': [{'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0},
+                    {'title': 'The Lord of the Functions: The Two Parameters', 'genre': 'Fantasy', 'rating': 4.0}, 
+                    {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, 
+                    {'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, 
+                    {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, 
+                    {'title': 'Instructor Student TA Manager', 'genre': 'Intrigue', 'rating': 4.5}]}
+
+print(get_most_watched_genre(user_data))
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
